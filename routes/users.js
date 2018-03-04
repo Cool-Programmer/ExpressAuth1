dab161eeccf6b043cc92a16d8487dc0a0c41cde3
The Cyool Programmer <iodevllc@gmail.com>
2018-03-04 22:26:13 +0400

    Done

---
+++ routes/users.js
@@ -0,0 +1,124 @@
+var express = require('express');
+var router = express.Router();
+var multer = require('multer');
+var upload = multer({dest: './uploads'});
+var User = require('../models/user');
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  res.send('respond with a resource');
+});
+
+// actually, /users/register
+router.get('/register', function(req, res, next) {
+  res.render('register');
+});
+
+router.post('/register', upload.single('profileimage'), function(req, res, next) {
+	var name = req.body.name;
+	var email = req.body.email;
+	var username = req.body.username;
+	var password = req.body.password;
+	var password2 = req.body.password2;
+
+	if (req.file) {
+		var profileimage = req.file.filename;
+	}else{
+		console.log('Something went wrong...');
+		var profileimage = 'noimage.jpg';
+	}
+
+	// Form validation
+	req.checkBody('name', 'Name field is required').notEmpty();
+	req.checkBody('email', 'Email field is required').notEmpty();
+	req.checkBody('email', 'Email is not valid').isEmail();
+	req.checkBody('username', 'Username is required').notEmpty();
+	req.checkBody('password', 'Password is required').notEmpty();
+	req.checkBody('password2', 'Passwords do not match').equals(req.body.password);
+
+
+	// Check for errors
+	var errors = req.validationErrors();
+	if (errors) {
+		res.render('register', {
+			errors: errors
+		});
+	}else{
+		var newUser = new User({
+			name: name,
+			email: email,
+			username: username,
+			password: password,
+			profileimage: profileimage
+		});
+
+		User.createUser(newUser, function(err, user){
+			if (err) {
+				throw err;
+				console.log(user)
+			}
+		});
+
+		req.flash('success', 'You are now registered and can log in!');
+
+		res.location('/');
+		res.redirect('/');
+	}
+});
+
+router.get('/login', function(req, res, next) {
+  res.render('login');
+});
+
+router.post('/login', passport.authenticate('local', {
+  failureRedirect: '/users/login',
+  failureFlash: 'Invalid username or password'
+}), function(req, res) {
+  console.log('Authentication Successful');
+  req.flash('success', 'You are logged in');
+  res.redirect('/');
+});
+
+passport.serializeUser(function(user, done){
+	done(null, user.id);
+});
+
+passport.deserializeUser(function(id, done){
+	User.getUserById(id, function(err, user){
+		done(err, user);
+	});
+});
+
+passport.use(new LocalStrategy(function(username, password, done){
+	User.getUserByUsername(username, function(err, user){
+		if (err) {
+			throw err;
+		}
+
+		if (!user) {
+			return done(null, false, {message: 'Unknown user'});
+		}
+
+		User.comparePassword(password, user.password, function(err, isMatch){
+			if (err) {
+				return done(err);
+			}
+
+			if (isMatch) {
+				return done(null, user);
+			}else{
+				return done(null, false, {message: 'Invalid credentials'});
+			}
+		});
+	});
+}));
+
+router.get('/logout', function(req, res){
+	req.logout();
+	req.flash('success', 'You are now logged out');
+	res.redirect('/users/login');
+});
+
+module.exports = router
